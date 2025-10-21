76d98de4fa957f3e35d37ade165ce47c2a731060
nmorcab2106 <amorac@iesrafaelalberti.es>
2025-10-21 18:45:33 +0200

    Rehacer la suma de 3 numeros, pero con 2 variables

---
+++ suma_2_variables.py
@@ -0,0 +1,14 @@
+num1 = input("Introduce el primer número: ")
+num1 = int(num1)
+
+num2 = input("Introduce el segundo número: ")
+num2 = int(num2)
+
+num1 = num1 + num2
+
+num2 = input("Introduce el tercer número: ")
+num2 = int(num2)
+
+num1 = num1 + num2
+
+print(f"La suma es: {num1}")

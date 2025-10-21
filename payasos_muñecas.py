0bb71b2bccd2050d04eb961001b30544a7fe976d
nmorcab2106 <amorac@iesrafaelalberti.es>
2025-10-21 19:17:12 +0200

    Calculo del peso total de un pedido

---
+++ payasos_muñecas.py
@@ -0,0 +1,9 @@
+payasos = input("Introduce el número de payasos vendidos: ")
+payasos = int(payasos)
+
+muñecas = input("Introduce el número de muñecas vendidas: ")
+muñecas = int(muñecas)
+
+peso_total = (payasos*112)+(muñecas*75)
+
+print(f"El peso total del paquete es {peso_total} gramos")

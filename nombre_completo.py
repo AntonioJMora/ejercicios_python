eaaf6283042079f46f7f5b7d2b02e5dbb2751c1f
nmorcab2106 <amorac@iesrafaelalberti.es>
2025-10-21 19:47:32 +0200

    Mostrar el nombre en minúsculas, mayúsculas y con mayúsculas iniciales

---
+++ nombre_completo.py
@@ -0,0 +1,5 @@
+nombre_completo = input("Introduce tu nombre completo: ")
+
+print(f"En minúsculas: {nombre_completo.lower()}")
+print(f"En mayúsculas: {nombre_completo.upper()}")
+print(f"Con mayúsculas iniciales: {nombre_completo.capitalize()}")

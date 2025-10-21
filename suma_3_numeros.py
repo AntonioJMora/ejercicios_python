9f09004b6f77809f77f0914e07916d3382bf023a
nmorcab2106 <amorac@iesrafaelalberti.es>
2025-10-21 18:37:34 +0200

    Suma de 3 números

---
+++ suma_3_numeros.py
@@ -0,0 +1,12 @@
+num1 = input("Introduce el primer número: ")
+num1 = int(num1)
+
+num2 = input("Introduce el segundo número: ")
+num2 = int(num2)
+
+num3 = input("Introduce el tercer número: ")
+num3 = int(num3)
+
+suma = num1 + num2 + num3
+
+print(f"La suma es: {suma}")

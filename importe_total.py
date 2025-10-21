21c8192b186133e443a1b8ff2505bb6bd95ea963
nmorcab2106 <amorac@iesrafaelalberti.es>
2025-10-21 17:56:16 +0200

    Calcular el importe total de un servicio

---
+++ importe_total.py
@@ -0,0 +1,6 @@
+horas = input("Introduzca las horas de trabajo: ")
+coste_hora = input("Introduce el coste por hora: ")
+
+total = int(horas) * int(coste_hora)
+
+print(f"El importe total es: {total}")

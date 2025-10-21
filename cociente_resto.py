2a38deae2f6d8c8133c6f41fda844da5ad7f4e67
nmorcab2106 <amorac@iesrafaelalberti.es>
2025-10-21 19:11:50 +0200

    Cociente y resto de una division

---
+++ cociente_resto.py
@@ -0,0 +1,10 @@
+n = input("Introduce el primer número: ")
+n = int(n)
+
+m = input("Introduce el segundo número: ")
+m = int(m)
+
+cociente = n // m
+resto = n % m
+
+print(f"La división de {n} entre {m} da un cociente de {cociente} y un resto de {resto}")

4fade19f36c4a0c4dc97bc6b829a824b7cb74737
nmorcab2106 <amorac@iesrafaelalberti.es>
2025-10-21 18:48:49 +0200

    Rehacer la suma de 3 numeros, pero sin variables adicionales

---
+++ suma_sin_variables.py
@@ -0,0 +1,10 @@
+num1 = input("Introduce el primer número: ")
+num1 = int(num1)
+
+num2 = input("Introduce el segundo número: ")
+num2 = int(num2)
+
+num3 = input("Introduce el tercer número: ")
+num3 = int(num3)
+
+print("La suma es: ",num1 + num2 + num3)

04fb8b9b109929310070aacaf39a475438244dbd
nmorcab2106 <amorac@iesrafaelalberti.es>
2025-10-22 19:07:51 +0200

    Mostrar el nombre de un producto, su precio y el número de unidades

--- producto_precio_unidades.py
+++ producto_precio_unidades.py
@@ -10,6 +10,6 @@ unidades = int(unidades)
 coste_total = precio * unidades
 
 print(f"Producto: {nombre_producto}")
-print(f"Precio unitario: {precio}")
-print(f"Unidades: {unidades}")
-print(f"Coste total: {coste_total}")
+print(f"Precio unitario: {precio:6.2f}")
+print(f"Unidades: {unidades:03d}")
+print(f"Coste total: {coste_total:08.2f}")
